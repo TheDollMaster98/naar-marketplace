cb11211024271cdf5a4adbbd368ddcc421257676
Loris <78973637+TheDollMaster98@users.noreply.github.com>
2025-04-27 19:36:54 +0200

    feat(login): add login page with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home-page/home.page';
 import { RegisterPage } from './pages/register-page/register.page';
 import { ProductsPage } from './pages/products/products.page';
+import { LoginPage } from './pages/login/login.page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
@@ -9,6 +11,7 @@ export const routes: Routes = [
   //   { path: 'home', loadComponent: () => import('./pages/home.page').then(m => m.HomePage) },
   { path: 'home', component: HomePage },
   { path: 'products', component: ProductsPage },
+  { path: 'login', component: LoginPage },
   { path: 'register', component: RegisterPage },
   { path: '**', redirectTo: '' }, //TODO: da fare pagina 404
 ];
