b520f06f758576e7e2cf7b855eaaabb4a745dae9
Loris <78973637+TheDollMaster98@users.noreply.github.com>
2025-05-01 23:55:05 +0200

    feat(carousel): created CarouselScrollComponent

---
+++ src/app/components/carousel-scroll/carousel-scroll/carousel-scroll.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel-scroll',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel-scroll.component.html',
+  styleUrl: './carousel-scroll.component.css'
+})
+export class CarouselScrollComponent {
+
+}
